c0aaa9a8b9585e4da56dc234d673400ee8ca2c75
Carole Gérard <gerard.carole78@gmail.com>
2023-11-06 10:37:42 +0100

    adding the 'assets' folder

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackers poulette</title>
+    <link rel="stylesheet" href="assets/css/style.css" type="text/css" charset="utf-8" />
+</head>
+<body>
+    <h1>Hackers poulette</h1>
+    <img src="assets/images/logos/hackers-poulette-logo.png" alt="Logo de 'Hackers poulette'">
+</body>
+</html>
